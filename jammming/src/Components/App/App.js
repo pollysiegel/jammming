57c3649e4b15c206b7ff2e4fb892c70a5a0887b5
psiegel <psiegel@atypon.com>
2018-03-17 16:14:53 -0700

    Finished through step 61 -- added playlist name change

--- jammming/src/Components/App/App.js
+++ jammming/src/Components/App/App.js
@@ -7,69 +7,76 @@ import './App.css';
 class App extends React.Component {
 
   constructor(props) {
-      super(props);
-      this.state =  {
-          searchResults: [{
-                  name: 'My Song',
-                  artist: 'Elton John',
-                  album: 'Madman Across the Water'
-              },
-              {
-                  name: 'Your Song',
-                  artist: 'Moody Blues',
-                  album: 'The Moody Blues'
-              }
-          ],
-          playlistName: 'My Playlist',
-          playlistTracks: [
-              {name:'Hello',
-               artist: 'You Guys',
-               album: 'Goodbye'
-                },
-              {name: 'Another brick in the wall',
-              artist: 'Pink Floyd',
-              album: 'The Wall'}
-              ]
-      };
-      this.addTrack = this.addTrack.bind(this);
-      this.removeTrack = this.removeTrack.bind(this);
+    super(props);
+    this.state =  {
+      searchResults: [{
+          name: 'My Song',
+          artist: 'Elton John',
+          album: 'Madman Across the Water'
+        },
+        {
+          name: 'Your Song',
+          artist: 'Moody Blues',
+          album: 'The Moody Blues'
+        }
+      ],
+      playlistName: 'My Playlist',
+      playlistTracks: [
+        {
+          name:'Hello',
+          artist: 'You Guys',
+          album: 'Goodbye'
+        },
+        {
+          name: 'Another brick in the wall',
+          artist: 'Pink Floyd',
+          album: 'The Wall'
+        }
+      ]
+    };
+    this.addTrack = this.addTrack.bind(this);
+    this.removeTrack = this.removeTrack.bind(this);
+    this.updatePlaylistName = this.updatePlaylistName.bind(this);
   }
 
-    addTrack(track) {
+  addTrack(track) {
 
-      /* TODO: Ask about id property of track. I have no idea what they're talking about */
+    /* TODO: Ask about id property of track. I have no idea what they're talking about */
 
-      let i = 0;
-      let found = 0;
-      while (i < this.state.playlistTracks.length && !found) {
-          if (this.state.playlistTracks[i].name === track.name) {
-              found = 1;  /* Track is already in the playlist */
-          }
-      }
-
-      /* If track isn't in the playlist add it */
-
-      if (!found) {
-          this.state.playlistTracks.push(track);
+    let i = 0;
+    let found = 0;
+    while (i < this.state.playlistTracks.length && !found) {
+      if (this.state.playlistTracks[i].name === track.name) {
+        found = 1;  /* Track is already in the playlist */
       }
     }
 
-    removeTrack(track) {
+    /* If track isn't in the playlist add it */
 
-        let i = 0;
-        let found = 0;
-        while (i < this.state.playlistTracks.length && !found) {
-            if (this.state.playlistTracks[i].name === track.name) {
-                found = 1;  /* Track is already in the playlist */
-            }
-        }
-
-        /* If we find the track in our playlist, remove it from the playlist */
-
-        if (found) {
-            this.state.playlistTracks.splice(i, 1);
-        }
+    if (!found) {
+      this.state.playlistTracks.push(track);
     }
+  }
+
+  removeTrack(track) {
+    let i = 0;
+    let found = 0;
+    while (i < this.state.playlistTracks.length && !found) {
+      if (this.state.playlistTracks[i].name === track.name) {
+        found = 1;  /* Track is already in the playlist */
+      }
+    }
+
+    /* If we find the track in our playlist, remove it from the playlist */
+
+    if (found) {
+      this.state.playlistTracks.splice(i, 1);
+    }
+  }
+
+  updatePlaylistName(name) {
+    this.state.playlistName = name;
+  }
 
   render() {
     return (
@@ -79,7 +86,8 @@ class App extends React.Component {
             <SearchBar />
             <div className="App-playlist">
               <SearchResults searchResults={this.state.searchResults} onAdd={this.addTrack} />
-              <Playlist playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks} onRemove={this.removeTrack} />
+              <Playlist playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks}
+                        onRemove={this.removeTrack} onNameChange={this.updatePlaylistName}/>
             </div>
           </div>
       </div>
--- jammming/src/Components/Playlist/Playlist.js
+++ jammming/src/Components/Playlist/Playlist.js
@@ -3,16 +3,30 @@ import TrackList from '../TrackList/TrackList';
 import './Playlist.css';
 
 class Playlist extends React.Component {
-    render() {
-        return (
-            <div className="Playlist">
-                <input defaultValue={'New Playlist'} />
-                <TrackList tracks={this.props.playlistTracks} onRemove={this.props.onRemove}  />
-                <a className="Playlist-save">SAVE TO SPOTIFY</a>
-            </div>
 
-        )
-    };
+  constructor(props) {
+    super(props);
+    this.handleNameChange = this.handleNameChange.bind(this);
+  }
+
+  /*
+   * Set Playlist name to new name.
+   */
+
+  handleNameChange(event) {
+    this.props.onNameChange(event.target.value);
+  }
+
+  render() {
+    return (
+      <div className="Playlist">
+        <input defaultValue={'New Playlist'} onChange={this.handleNameChange} />
+        <TrackList tracks={this.props.playlistTracks} onRemove={this.props.onRemove}  />
+        <a className="Playlist-save">SAVE TO SPOTIFY</a>
+      </div>
+
+    )
+  }
 }
 
 export default Playlist;
